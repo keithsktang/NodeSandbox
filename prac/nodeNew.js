a91c8dcbd573b80cf4faade1cf27105de5e47c77
Keith Tang <keithsktang@gmail.com>
2017-08-18 10:06:47 -0400

    add eslint

--- prac/nodeNew.js
+++ prac/nodeNew.js
@@ -7,23 +7,34 @@
 //     res.end();
 //   });
 // }).listen(8080);
+
+// testing code for omdb api
+
 var request = require('request');
 var url = require('url');
 var express = require('express');
-var app = express()
+var app = express();
 
-app.get('/:title', function (req, res) {
+app.get('/', function(req, res){
+  res.write('Hello world!');
+  res.end();
+});
+
+app.get('/movie/:title', function (req, res) {
   var options = {
     protocol: 'http:',
     host: 'www.omdb.com',
+    charset: 'UTF-8',
     query: {
-      t: req.params.name
+      t: req.params.title
     }
-  }
+  };
 
-  res.write("say this:");
-  var searhURL = url.format(options);
+  res.write('say this:');
+  var searchURL = url.format(options);
   request(searchURL).pipe(res);
 });
 
-app.listen(8080);
+app.listen(3000);
+
+// end test code for omdb api
