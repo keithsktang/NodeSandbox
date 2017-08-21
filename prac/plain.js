11e30e4f3d1ed97c282236d6304a238947853dc6
Keith Tang <keithsktang@gmail.com>
2017-08-21 15:43:00 -0400

    notde testing

---
+++ prac/plain.js
@@ -0,0 +1,10 @@
+// var func = function (){
+//   console.log('hello world');
+// };
+
+// func();
+
+var emp = {id: 1, manager: null};
+emp.manager = emp;
+JSON.stringify(emp);
+
--- prac/prac_express.js
+++ prac/prac_express.js
@@ -1,22 +1,10 @@
-// var http = require('http');
-// var fs = require('fs');
-// http.createServer(function (req, res) {
-//   fs.readFile('index.html', function(err, data) {
-//     res.writeHead(200, {'Content-Type': 'text/html'});
-//     res.write(data);
-//     res.end();
-//   });
-// }).listen(8080);
-
-// testing code for omdb api
-
 var request = require('request');
 var url = require('url');
 var express = require('express');
 var app = express();
 
 app.get('/', function(req, res){
-  res.write('Hello world!');
+  res.write(tyrion.bodyStyle);
   res.end();
 });
 
@@ -36,5 +24,10 @@ app.get('/movie/:title', function (req, res) {
 });
 
 app.listen(3000);
+
+let tyrion = new LeatherArmor('short');
+function LeatherArmor ( bodyStyle ){
+  return {bodyStyle : bodyStyle};
+}
 
 // end test code for omdb api
